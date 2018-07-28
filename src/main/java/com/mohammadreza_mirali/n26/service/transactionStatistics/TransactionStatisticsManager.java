bdbac2d72f6ffeb37ccbda3906b38f8c21378f82
Mohamadreza Mirali <smrm1363@gmail.com>
2018-07-28 11:20:34 +0430

    new commit, test complete

--- src/main/java/com/mohammadreza_mirali/n26/service/transactionStatistics/TransactionStatisticsManager.java
+++ src/main/java/com/mohammadreza_mirali/n26/service/transactionStatistics/TransactionStatisticsManager.java
@@ -5,13 +5,17 @@ import com.mohammadreza_mirali.n26.service.transactionStatistics.dto.Transaction
 import javax.inject.Named;
 import java.time.Instant;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 @Named("TransactionStatisticsManager")
 public class TransactionStatisticsManager {
 
     private static final int validityTime= 60000;
-    private List<TransactionDto> transactionDtoList = new ArrayList<>();
+    private List<TransactionDto> transactionDtoList = Collections.synchronizedList(new ArrayList<TransactionDto>());
+    private StatisticDto statisticDto = new StatisticDto();
+    private Boolean threadStarted = false;
 
     public synchronized String transactions(TransactionDto transactionDto)
     {
@@ -19,6 +23,14 @@ public class TransactionStatisticsManager {
         if(transactionDto.getTimestamp() >= Instant.now().toEpochMilli() - validityTime)
         {
             transactionDtoList.add(transactionDto);
+
+            if(threadStarted == false) {
+                ExecutorService executor = Executors.newSingleThreadExecutor();
+                threadStarted = true;
+                executor.submit(() -> {gatherStatistics(transactionDtoList);
+
+                });
+            }
             return "201";
 
         }
@@ -28,22 +40,31 @@ public class TransactionStatisticsManager {
 
     public synchronized  StatisticDto statistics()
     {
-        StatisticDto statisticDto = new StatisticDto();
+
+        return statisticDto;
+    }
+
+    private void gatherStatistics(List<TransactionDto> transactionDtoList)
+    {
+
         Comparator<TransactionDto> comparator = Comparator.comparing(TransactionDto::getAmount);
+        while(true){
         if(transactionDtoList.size() > 0) {
             List<TransactionDto> transactionDtoFilteredList = transactionDtoList.parallelStream().
                     filter(p -> p.getTimestamp() > Instant.now().toEpochMilli() - validityTime).collect(Collectors.toList());
             if(transactionDtoFilteredList.size()==0)
-                return statisticDto;
+                return ;
             statisticDto.setMax(transactionDtoFilteredList.stream().max(comparator).get().getAmount());
             statisticDto.setMin(transactionDtoFilteredList.stream().min(comparator).get().getAmount());
             statisticDto.setCount(transactionDtoFilteredList.stream().count());
             statisticDto.setAvg(transactionDtoFilteredList.stream().mapToDouble(o -> o.getAmount()).average().getAsDouble());
             statisticDto.setSum(transactionDtoFilteredList.stream().mapToDouble(o -> o.getAmount()).sum());
-            transactionDtoList = transactionDtoFilteredList;
-        }
-        return statisticDto;
+            transactionDtoList =  Collections.synchronizedList(new ArrayList<TransactionDto>(transactionDtoFilteredList));
+            System.out.println("runn..."+transactionDtoList.size());
+        }}
+
     }
+
 
     public List<TransactionDto> getTransactionDtoList() {
         return transactionDtoList;
